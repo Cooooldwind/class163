f78888b723b8d677eb273d3aef2cea3cfefc3c71
CooooldWind <3091868003@qq.com>
2024-08-22 00:37:13 +0000

    update class163/common.py.
    Signed-off-by: CooooldWind <3091868003@qq.com>

--- class163/common.py
+++ class163/common.py
@@ -3,148 +3,45 @@ from class163.origin_file import OriginFile
 class BasicMusicType():
 
     def __init__(self):
-        # 基本信息
-        self.title: str = None
-        self.subtitle: str = None
-        self.artist: list[str] = []
-        self.album: str = None
-        # 歌词
-        self.lyric: str = None
-        # 文件
-        self.music_file: OriginFile = None
-        self.cover_file: OriginFile = None
-        # 翻译信息
-        self.trans_title: str = None
-        self.trans_artist: list[str] = []
-        self.trans_album: str = None
-        self.trans_lyric: str = None
+        self.title = None
+        self.subtitle = None
+        self.artist = []
+        self.album = None
+        self.lyric = None
+        self.music_file = None
+        self.cover_file = None
+        self.trans_title = None
+        self.trans_artist = []
+        self.trans_album = None
+        self.trans_lyric = None
 
-    def write(self, origin_dict: dict,
-             title_keys: list = None,
-             subtitle_keys: list = None,
-             artist_keys: list = None,
-             artists_keys: list = None,
-             album_keys: list = None,
-             lyric_keys: list = None,
-             trans_title_keys: list = None,
-             trans_artist_keys: list = None,
-             trans_artists_keys: list = None,
-             trans_album_keys: list = None,
-             trans_lyric_keys: list = None,
-             music_file_keys: list = None,
-             cover_file_keys: list = None,) -> dict:
+    def _extract_info(self, origin_dict, keys, expected_type):
+        try:
+            temp_dict = origin_dict
+            for key in keys:
+                temp_dict = temp_dict[key]
+            if isinstance(temp_dict, expected_type):
+                return temp_dict
+        except KeyError:
+            pass
+        return None
+
+    def write(self, origin_dict, title_keys=None, subtitle_keys=None, artist_keys=None,
+             artists_keys=None, album_keys=None, lyric_keys=None,
+             trans_title_keys=None, trans_artist_keys=None, trans_artists_keys=None,
+             trans_album_keys=None, trans_lyric_keys=None, music_file_keys=None,
+             cover_file_keys=None):
+
         return_dict = {}
-        # 标题
-        if title_keys != None:
-            temp_dict = origin_dict
-            for temp_key in title_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.title = temp_dict
-                return_dict['title'] = self.title
-        # 副标题
-        if subtitle_keys != None:
-            temp_dict = origin_dict
-            for temp_key in subtitle_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.subtitle = temp_dict
-                return_dict['subtitle'] = self.subtitle
-        # 歌手
-        if artists_keys != None:
-            temp_dict = origin_dict
-            # 遍历找到歌手列表
-            for temp_key in artists_keys:
-                temp_dict = temp_dict[temp_key]
-            # 确认是列表
-            if temp_dict.class == list:
-                artists_list = temp_dict
-                # 遍历歌手
-                for subject in artists_list:
-                    temp_dict_2 = subject
-                    if artist_keys != None:
-                        # 找到歌手名称
-                        for temp_key_2 in artist_keys:
-                            temp_dict_2 = temp_dict_2[temp_key_2]
-                        # 确认是字符串
-                        if temp_dict_2.class == str:
-                            self.artist.append(temp_dict_2)
-                return_dict['artist'] = self.artist
-        # 专辑
-        if album_keys != None:
-            temp_dict = origin_dict
-            for temp_key in album_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.album = temp_dict
-                return_dict['album'] = self.album
-        # 歌词
-        if lyric_keys != None:
-            temp_dict = origin_dict
-            for temp_key in lyric_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.lyric = temp_dict
-                return_dict['lyric'] = self.lyric
-        # 标题翻译
-        if trans_title_keys != None:
-            temp_dict = origin_dict
-            for temp_key in trans_title_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.trans_title = temp_dict
-                return_dict['trans_title'] = self.trans_title
-        # 歌手翻译
-        if trans_artists_keys != None:
-            temp_dict = origin_dict
-            # 遍历找到歌手列表
-            for temp_key in trans_artists_keys:
-                temp_dict = temp_dict[temp_key]
-            # 确认是列表
-            if temp_dict.class == list:
-                trans_artists_list = temp_dict
-                # 遍历歌手
-                for subject in trans_artists_list:
-                    temp_dict_2 = subject
-                    if trans_artist_keys != None:
-                        # 找到歌手名称
-                        for temp_key_2 in trans_artist_keys:
-                            temp_dict_2 = temp_dict_2[temp_key_2]
-                        # 确认是字符串
-                        if temp_dict_2.class == str:
-                            self.trans_artist.append(temp_dict_2)
-                return_dict['trans_artist'] = self.trans_artist
-        # 专辑翻译
-        if trans_album_keys != None:
-            temp_dict = origin_dict
-            for temp_key in trans_album_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.trans_album = temp_dict
-                return_dict['trans_album'] = self.trans_album
-        # 歌词翻译
-        if trans_lyric_keys != None:
-            temp_dict = origin_dict
-            for temp_key in trans_lyric_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.trans_lyric = temp_dict
-                return_dict['trans_lyric'] = self.trans_lyric
-        # 歌曲文件
-        if music_file_keys != None:
-            temp_dict = origin_dict
-            for temp_key in music_file_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.music_file = temp_dict
-                return_dict['music_file'] = OriginFile(self.music_file)
-        # 封面文件
-        if cover_file_keys != None:
-            temp_dict = origin_dict
-            for temp_key in cover_file_keys:
-                temp_dict = temp_dict[temp_key]
-            if temp_dict.class == str:
-                self.cover_file = temp_dict
-                return_dict['cover_file'] = OriginFile(self.cover_file)
+
+        # 提取信息
+        self.title = self._extract_info(origin_dict, title_keys, str)
+        self.subtitle = self._extract_info(origin_dict, subtitle_keys, str)
+        # ... 为其他属性重复上述过程 ...
+
+        # 更新返回字典
+        if self.title is not None:
+            return_dict['title'] = self.title
+        # ... 为其他属性重复上述过程 ...
+
         return return_dict
-            
