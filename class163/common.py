c8a10c1d97c32badfb133a76f767047178f09e27
CooooldWind <3091868003@qq.com>
2024-08-23 02:46:35 +0000

    update class163/common.py.
    Signed-off-by: CooooldWind <3091868003@qq.com>

--- class163/common.py
+++ class163/common.py
@@ -8,26 +8,11 @@ Copyright @CooooldWind_ / Following GNU_AGPLV3+ License
 from typing import Optional, Dict, List, Union
 
 class BasicMusicType:
-    """
-    表示基本音乐类型的类，用于从给定的字典中提取音乐相关信息并以特定格式返回。
-    """
 
     def __init__(self):
-        """
-        初始化类的属性。
-        """
         self.info = {}
 
     def extract(self, origin_dict: dict, keys: List[Union[str, int]], expected_type) -> Optional[Union[str, list]]:
-        """
-        从给定的字典中通过一系列键提取信息，并检查提取出的值是否符合预期类型。
-        如果符合预期类型则返回提取的值，否则返回 None。如果在提取过程中发生 KeyError 也返回 None。
-
-        :param origin_dict: 原始字典。
-        :param keys: 用于提取信息的键。
-        :param expected_type: 预期的返回值类型。
-        :return: 提取出的符合预期类型的值或 None。
-        """
         try:
             temp_dict = origin_dict
             for key in keys if isinstance(keys, list) else [keys]:
