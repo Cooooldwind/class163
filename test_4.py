37e8aff1957476ea092226ddcf7ba4a924ca3329
CoooldWind <3091868003@qq.com>
2024-08-27 07:28:11 +0800

    0.6.0

--- test_4.py
+++ test_4.py
@@ -2,7 +2,18 @@
 from class163.music import Music
 from pprint import pprint
 pprint(Music("https://music.163.com/song?id=1486506463&uct2=U2FsdGVkX19PImPHgQ0/67dW7Hj62Hj3yxx/OEBK8pA=").get_detail())
-"""
+
 from class163.playlist import Playlist
 from pprint import pprint
 pprint(Playlist(12323740535).get_detail())
+"""
+
+from class163.search import Search
+import json
+
+U = "0A0DEADBEEF114514415411FEEDDAED0A0" # 但凡有点脑子就知道我不可能把我cookies发出来
+with open("test.json", "w", encoding="UTF-8") as f:
+    json.dump(
+        Search(key="明透", search_type="song", cookie_dict={"MUSIC_U": U}).get(),
+        fp=f, ensure_ascii=False
+    )
